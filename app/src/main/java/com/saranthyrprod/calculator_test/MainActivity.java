3bd406d8806dacafda646e246af79d1170c86c65
Maksim Maksimets <kirillmaksimets@gmail.com>
2021-01-01 16:54:07 +0300

    Basic Calculator V.1 done

--- app/src/main/java/com/saranthyrprod/calculator_test/MainActivity.java
+++ app/src/main/java/com/saranthyrprod/calculator_test/MainActivity.java
@@ -3,6 +3,11 @@ package com.saranthyrprod.calculator_test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,71 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        EditText eNT = findViewById(R.id.editTextNumber);
+        Button b = (Button) view;
+        eNT.setText(eNT.getText().toString() + b.getText().toString());
+    }
+
+    public void onClick2(View view) {
+        EditText eNT = findViewById(R.id.editTextNumber);
+        Button b = (Button) view;
+        if (eNT.getText().toString().endsWith("/") || eNT.getText().toString().endsWith("+") || eNT.getText().toString().endsWith("-") || eNT.getText().toString().endsWith("*")) {
+            eNT.setText(eNT.getText().toString().replaceAll(".$", ""));
+            eNT.setText(eNT.getText().toString() + b.getText().toString());
+        }
+        else {
+            eNT.setText(eNT.getText().toString() + b.getText().toString());
+        }
+    }
+
+    public void Result(View view) {
+        EditText eNT = findViewById(R.id.editTextNumber);
+        String expression = eNT.getText().toString();
+        if (expression.contains("+")) {
+            String[] temp = expression.split("\\+");
+            float a = Float.parseFloat(temp[0]);
+            float b = Float.parseFloat(temp[1]);
+            float result = a+b;
+            eNT.setText(Float.toString(result));
+        }
+        else if (expression.contains("-")) {
+            String[] temp = expression.split("-");
+            float a = Float.parseFloat(temp[0]);
+            float b = Float.parseFloat(temp[1]);
+            float result = a-b;
+            eNT.setText(Float.toString(result));
+        }
+        else if (expression.contains("*")) {
+            String[] temp = expression.split("\\*");
+            float a = Float.parseFloat(temp[0]);
+            float b = Float.parseFloat(temp[1]);
+            float result = a*b;
+            eNT.setText(Float.toString(result));
+        }
+        else if (expression.contains("/")) {
+            String[] temp = expression.split("/");
+            float a = Float.parseFloat(temp[0]);
+            float b = Float.parseFloat(temp[1]);
+            float result = a/b;
+            eNT.setText(Float.toString(result));
+        }
+        if (eNT.getText().toString().endsWith(".0")) {
+            String[] temp1 = eNT.getText().toString().split("\\.");
+            eNT.setText(temp1[0]);
+        }
+
+    }
+
+    public void removeLast(View view) {
+        EditText eNT = findViewById(R.id.editTextNumber);
+        eNT.setText(eNT.getText().toString().replaceAll(".$", ""));
+    }
+
+    public void Clear(View view) {
+        EditText eNT = findViewById(R.id.editTextNumber);
+        eNT.setText("");
     }
 }
