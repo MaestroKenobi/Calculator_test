a9ebb1cf3476a07c41664bba0e1d5ed5c11cb83d
Maksim Maksimets <kirillmaksimets@gmail.com>
2021-01-02 20:06:18 +0300

    Multiple variables allowed

--- app/src/main/java/com/saranthyrprod/calculator_test/MainActivity.java
+++ app/src/main/java/com/saranthyrprod/calculator_test/MainActivity.java
@@ -3,11 +3,13 @@ package com.saranthyrprod.calculator_test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.content.Intent;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,9 +28,15 @@ public class MainActivity extends AppCompatActivity {
     public void onClick2(View view) {
         EditText eNT = findViewById(R.id.editTextNumber);
         Button b = (Button) view;
+        int a = eNT.getText().toString().length();
         if (eNT.getText().toString().endsWith("/") || eNT.getText().toString().endsWith("+") || eNT.getText().toString().endsWith("-") || eNT.getText().toString().endsWith("*")) {
-            eNT.setText(eNT.getText().toString().replaceAll(".$", ""));
-            eNT.setText(eNT.getText().toString() + b.getText().toString());
+            if (a == 1 & eNT.getText().toString().startsWith("-")) {
+                eNT.setText(eNT.getText().toString() + b.getText().toString());
+            }
+            else {
+                eNT.setText(eNT.getText().toString().replaceAll(".$", ""));
+                eNT.setText(eNT.getText().toString() + b.getText().toString());
+            }
         }
         else {
             eNT.setText(eNT.getText().toString() + b.getText().toString());
@@ -38,34 +46,99 @@ public class MainActivity extends AppCompatActivity {
     public void Result(View view) {
         EditText eNT = findViewById(R.id.editTextNumber);
         String expression = eNT.getText().toString();
-        if (expression.contains("+")) {
-            String[] temp = expression.split("\\+");
-            float a = Float.parseFloat(temp[0]);
-            float b = Float.parseFloat(temp[1]);
-            float result = a+b;
-            eNT.setText(Float.toString(result));
+        ArrayList<Float> test = new ArrayList<>();
+        float result = 0;
+        String[] temp = expression.split("\\+|\\*|/|-");
+        String temp2 = expression.replaceAll("[0-9]|\\.", "");
+        char[] tmp1 = temp2.toCharArray();
+        List<Character> actions = new ArrayList<>();
+        for (char c : tmp1) {
+            actions.add(c);
         }
-        else if (expression.contains("-")) {
-            String[] temp = expression.split("-");
-            float a = Float.parseFloat(temp[0]);
-            float b = Float.parseFloat(temp[1]);
-            float result = a-b;
-            eNT.setText(Float.toString(result));
+        int j = actions.size();
+        while (actions.isEmpty() == false && test.size() > 1) {
+            for (int i = 0; i < j; ) {
+                if (actions.get(i) == '*') {
+                    float tmp = test.get(i) * test.get(i + 1);
+                    test.set(i, tmp);
+                    test.remove(i + 1);
+                    actions.remove(i);
+                    i = i;
+                } else if (actions.get(i) == '/') {
+                    float tmp = test.get(i) / test.get(i + 1);
+                    test.set(i, tmp);
+                    test.remove(i + 1);
+                    actions.remove(i);
+                    i = i;
+                } else {
+                    i++;
+                }
+            }
+            for (int i = 0; i < j;)  {
+                if (actions.get(i) == '+') {
+                    float tmp = test.get(i) + test.get(i + 1);
+                    test.set(i, tmp);
+                    test.remove(i + 1);
+                    actions.remove(i);
+                    i = i;
+                } else if (actions.get(i) == '-') {
+                    float tmp = test.get(i) - test.get(i + 1);
+                    test.set(i, tmp);
+                    test.remove(i + 1);
+                    actions.remove(i);
+                    i = i;
+                } else {
+                    i++;
+                }
+            }
         }
-        else if (expression.contains("*")) {
-            String[] temp = expression.split("\\*");
-            float a = Float.parseFloat(temp[0]);
-            float b = Float.parseFloat(temp[1]);
-            float result = a*b;
-            eNT.setText(Float.toString(result));
-        }
-        else if (expression.contains("/")) {
-            String[] temp = expression.split("/");
-            float a = Float.parseFloat(temp[0]);
-            float b = Float.parseFloat(temp[1]);
-            float result = a/b;
-            eNT.setText(Float.toString(result));
-        }
+        result = test.get(0);
+        eNT.setText(Float.toString(result));
+//        if (expression.contains("+")) {
+//            String[] temp = expression.split("\\+");
+//            int tempL = temp.length;
+//            for (int i = 0; i < tempL; i++) {
+//                test.add(Float.parseFloat(temp[i]));
+//            }
+//            for (int i = 0; i < test.size(); i++) {
+//                result = result + test.get(i);
+//            }
+//            eNT.setText(Float.toString(result));
+//        }
+//        else if (expression.contains("-")) {
+//            String[] temp = expression.split("-");
+//            int tempL = temp.length;
+//            for (int i = 0; i < tempL; i++) {
+//                test.add(Float.parseFloat(temp[i]));
+//            }
+//            result = result + test.get(0);
+//            for (int i = 1; i < test.size(); i++) {
+//                result = result - test.get(i);
+//            }
+//            eNT.setText(Float.toString(result));
+//        }
+//        else if (expression.contains("*")) {
+//            String[] temp = expression.split("\\*");
+//            int tempL = temp.length;
+//            for (int i = 0; i < tempL; i++) {
+//                test.add(Float.parseFloat(temp[i]));
+//            }
+//            for (int i = 0; i < test.size(); i++) {
+//                result = result * test.get(i);
+//            }
+//            eNT.setText(Float.toString(result));
+//        }
+//        else if (expression.contains("/")) {
+//            String[] temp = expression.split("/");
+//            int tempL = temp.length;
+//            for (int i = 0; i < tempL; i++) {
+//                test.add(Float.parseFloat(temp[i]));
+//            }
+//            for (int i = 0; i < test.size(); i++) {
+//                result = result / test.get(i);
+//            }
+//            eNT.setText(Float.toString(result));
+//        }
         if (eNT.getText().toString().endsWith(".0")) {
             String[] temp1 = eNT.getText().toString().split("\\.");
             eNT.setText(temp1[0]);
